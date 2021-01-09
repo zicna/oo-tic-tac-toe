8a78f25689dafe18664938e8e745ed12d8da5d1e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-01-09 11:13:58 -0600

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class TicTacToe
 
     def initialize
@@ -25,19 +27,20 @@ class TicTacToe
 
     def input_to_index(input)
         input = input.to_i
-        @index = input - 1
+        #*******************************************************************
+        #1. shold we set index as a instance variable or method variable?
+        index = input - 1
     end
-
     def move(index, token = "X")
         @board[index] = token
     end
 
     def position_taken?(index)
-        @board[index] == " " ? false : true
+        @board[index] == " " || @board[index] == nil ? false : true
     end
 
     def valid_move?(index)
-        index.between?(0, 8) && @board[index] == " " ? true : false
+        index.between?(0, 8) && !position_taken?(index) ? true : false
     end
 #can we do this in fewer line of code???
     def turn_count
@@ -54,12 +57,90 @@ class TicTacToe
         turn_count
         @count == 0 || @count.even? ? "X" : "O"
     end
-
+    #*******************************************************************
+    #2. best way to call other instance method?
     def turn
-        ##input_to_index, #valid_move?, and #current_player"
-        puts "please choose your move."
+        puts "Please choose a number 1-9:"
         input = gets.chomp
-        input_to_index(input)
+        #binding.pry
+        index = input_to_index(input)
+        if valid_move?(index)
+            #*******************************************************************
+            #3. can this be done with just calling current_player as argument?
+            token = current_player
+            move(index, token)
+            display_board
+        else
+            turn
+        end
     end
+
+    def won?
+        x_combinations = []
+        o_combinations = []
+        @final = []
+        @board.each.with_index do |token, index|
+            if token == "X"
+                x_combinations << index
+            elsif token == "O"
+                o_combinations << index
+            end
+        end
+        
+        WIN_COMBINATIONS.each do |comb|
+            #binding.pry
+            if comb.all? {|item| o_combinations.include?(item)}
+                @final = comb
+                #4. why is this break making this to pass or why is this "break" necessary
+                break
+            elsif comb.all? {|item| x_combinations.include?(item)}
+                @final = comb
+                break
+            else
+                @final = false
+            end
+        end
+        @final
+    end
+
+    def full?
+        turn_count == 9 ? true : false
+    end
+
+    def draw?
+        full? && won? == false ? true : false
+    end
+
+    def over? 
+        won? || full? ? true : false
+    end
+    #option 1. won? method return a instance variable (I changed final => @final in won? method) and then operate on them
+    def winner
+        winner_token = ""
+        if won?
+            if @final.all? {|index| @board[index] == "X"}
+                winner_token = "X"
+            else
+                winner_token = "O"
+            end
+        else
+            winner_token = nil
+        end
+        winner_token
+    end
+    
+    def play
+        until over? == true
+            turn
+        end
+            
+        if won? 
+            puts "Congratulations #{winner}!"
+        elsif draw? 
+            puts "Cat's Game!"
+        end
+    
+    end
+
 
 end
